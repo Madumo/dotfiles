a252996d9702668967f60ed9671822f13851dcf1
Mathieu Dumont <dumont.m@icloud.com>
2024-10-12 13:34:04 -0400

    Add jankyborders

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -14,34 +14,34 @@ config.window_background_opacity = 0.85
 config.macos_window_background_blur = 20
 
 config.keys = {
-  {
-    key = "=",
-    mods = "SUPER",
-    action = wezterm.action.DisableDefaultAssignment,
-  },
+	{
+		key = "=",
+		mods = "SUPER",
+		action = wezterm.action.DisableDefaultAssignment,
+	},
 }
 
 config.inactive_pane_hsb = {
-  saturation = 0.9,
-  brightness = 0.8,
+	saturation = 0.9,
+	brightness = 0.8,
 }
 
-wezterm.on("window-focus-changed", function(window, pane)
-  local overrides = window:get_config_overrides() or {}
-
-  if window:is_focused() then
-    overrides.foreground_text_hsb = {
-      saturation = 1,
-      brightness = 1,
-    }
-  else
-    overrides.foreground_text_hsb = {
-      saturation = 0.8,
-      brightness = 0.7,
-    }
-  end
-
-  window:set_config_overrides(overrides)
-end)
+-- wezterm.on("window-focus-changed", function(window, pane)
+--	local overrides = window:get_config_overrides() or {}
+--
+--	if window:is_focused() then
+--		overrides.foreground_text_hsb = {
+--			saturation = 1,
+--			brightness = 1,
+--		}
+--	else
+--		overrides.foreground_text_hsb = {
+--			saturation = 0.8,
+--			brightness = 0.7,
+--		}
+--	end
+--
+--	window:set_config_overrides(overrides)
+-- end)
 
 return config
