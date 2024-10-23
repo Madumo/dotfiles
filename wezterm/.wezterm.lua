94004c02fd5d298e1a02672713938229512f502a
Mathieu Dumont <dumont.m@icloud.com>
2024-10-22 22:57:44 -0400

    Replace FiraCode with JetBrainsMono. I have to admit, it's just better.

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -2,7 +2,7 @@ local wezterm = require("wezterm")
 
 local config = wezterm.config_builder()
 
-config.font = wezterm.font("FiraCode Nerd Font Mono")
+config.font = wezterm.font("JetBrainsMono Nerd Font")
 config.font_size = 20
 
 config.enable_tab_bar = false
